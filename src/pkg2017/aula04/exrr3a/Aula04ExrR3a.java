e33125433d7f80341d451e4bd1824a8e267c9fc0
andremagno06 <andremagno06@gmail.com>
2017-08-17 22:39:07 -0300

    Updade exercico

---
+++ src/pkg2017/aula04/exrr3a/Aula04ExrR3a.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017.aula04.exrr3a;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ice
+ */
+public class Aula04ExrR3a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JanelaMat janela= new JanelaMat();
+        janela.setSize(500,500);
+        janela.setLocationRelativeTo(null);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setVisible(true);
+        
+    }
+    
+}
---
+++ src/pkg2017/aula04/exrr3a/JanelaMat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2017.aula04.exrr3a;
+
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ice
+ */
+class JanelaMat extends JFrame {
+    
+    
+    
+
+    public JanelaMat() throws HeadlessException {
+
+        super("Mat");
+        setLayout(new FlowLayout());
+        
+
+    }
+
+}
